8f7bbe27054eef560eb9fa39e700bb46512f0d6b
Teeshani <malishateeshani@gmail.com>
2025-01-07 19:57:14 +0530

    Complete contact us page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,40 +1,68 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import ContactImg from "../assets/images/cont-img.jpg";
 import "./Contact.css";
 import Footer from "./Footer";
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "289801d0-814f-4bca-b89d-8efe1f3455c8");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
+
   return (
     <>
     <Navbar />
+
+    <Hero
+  cName="hero-mid"
+  heroImg={ContactImg}
+  title="Contact Us"
+  />
+
     <section className="contact">
-    <h1 class="heading">
-          <span>contact</span> us
-        </h1>
         <div class="row">
-        <iframe
-            class="map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7930182.014594708!2d79.64764863975888!3d7.797226002369038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25092a5a5d8eb%3A0xcdfaa99e50e18064!2sSri%20Lanka!5e0!3m2!1sen!2s!4v1697903738920!5m2!1sen!2s"
-            allowfullscreen=""
-            loading="lazy"
-          ></iframe>
-          <form>
-          <h3>get in touch</h3>
+          <form onSubmit={onSubmit}>
+          <h3>we’re here to help you</h3>
           <div class="inputBox">
               <span class="fas fa-user"></span>
-              <input type="text" placeholder="name" />
-            </div>
-          <div class="inputBox">
-              <span class="fas fa-envelope"></span>
-              <input type="email" placeholder="email" />
+              <input type="text" name='name' placeholder="Enter your name" required/>
             </div>
           <div class="inputBox">
               <span class="fas fa-phone"></span>
-              <input type="number" placeholder="number" />
+              <input type="tel" name='phone' placeholder="Enter your mobile number" required/>
+            </div>
+          <div class="inputBox">
+              <span class="fas fa-envelope"></span>
+              <input type="message" placeholder="Enter your message" required/>
             </div>
           <input type="submit" value="contact now" class="btn" />
           </form>
+          <span>{result}</span>
         </div>
     </section>
     <Footer />
